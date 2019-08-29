2a420295b03255d48b85254928927c9b0de01fdb
RodrigoMattosoSilveira <r3850365@gmail.com>
2019-08-29 15:03:48 -0700

    basic-tests  -  Remove any Jasmine specific code from e2e/protractor.conf.js

--- e2e/protractor.conf.js
+++ e2e/protractor.conf.js
@@ -1,8 +1,6 @@
 // Protractor configuration file, see link for more information
 // https://github.com/angular/protractor/blob/master/lib/config.ts
 
-const { SpecReporter } = require('jasmine-spec-reporter');
-
 exports.config = {
   allScriptsTimeout: 11000,
   specs: [
@@ -18,15 +16,9 @@ exports.config = {
   cucumberOpts: {
    require: ['./src/steps/**/*.steps.ts'],
   },
-  jasmineNodeOpts: {
-    showColors: true,
-    defaultTimeoutInterval: 30000,
-    print: function() {}
-  },
   onPrepare() {
     require('ts-node').register({
       project: require('path').join(__dirname, './tsconfig.e2e.json')
     });
-    jasmine.getEnv().addReporter(new SpecReporter({ spec: { displayStacktrace: true } }));
   }
 };
